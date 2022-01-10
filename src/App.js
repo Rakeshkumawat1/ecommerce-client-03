89b5826eb7a51b85bc10fca6b418a60a7a132da8
Rakesh Kumawat <rakeskumarkumawat@gmail.com>
2022-01-11 05:28:05 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import HomePage from "./containers/HomePage";
-import ProductListPage from "./containers/ProductListPage";
-import { useDispatch, useSelector } from "react-redux";
-import { isUserLoggedIn, updateCart } from "./actions";
-import ProductDetailsPage from "./containers/ProductDetailsPage";
-import CartPage from "./containers/CartPage";
-import CheckoutPage from "./containers/CheckoutPage";
-import OrderPage from "./containers/OrderPage";
-import OrderDetailsPage from "./containers/OrderDetailsPage";
-import ViewProfile from "./containers/UserProfile/viewProfile"
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import HomePage from "./containers/HomePage";
+// import ProductListPage from "./containers/ProductListPage";
+// import { useDispatch, useSelector } from "react-redux";
+// import { isUserLoggedIn, updateCart } from "./actions";
+// import ProductDetailsPage from "./containers/ProductDetailsPage";
+// import CartPage from "./containers/CartPage";
+// import CheckoutPage from "./containers/CheckoutPage";
+// import OrderPage from "./containers/OrderPage";
+// import OrderDetailsPage from "./containers/OrderDetailsPage";
+// import ViewProfile from "./containers/UserProfile/viewProfile"
 
 function App() {
-  const dispatch = useDispatch();
-  const auth = useSelector((state) => state.auth);
+  // const dispatch = useDispatch();
+  // const auth = useSelector((state) => state.auth);
 
-  useEffect(() => {
-    if (!auth.authenticate) {
-      dispatch(isUserLoggedIn());
-    }
-  }, [auth.authenticate]);
+  // useEffect(() => {
+  //   if (!auth.authenticate) {
+  //     dispatch(isUserLoggedIn());
+  //   }
+  // }, [auth.authenticate]);
 
-  useEffect(() => {
-    console.log("App.js - updateCart");
-    dispatch(updateCart());
-  }, [auth.authenticate]);
+  // useEffect(() => {
+  //   console.log("App.js - updateCart");
+  //   dispatch(updateCart());
+  // }, [auth.authenticate]);
 
   return (
     <div>
-      <Router>
+      <h1>Test</h1>
+      {/* <Router>
         <Switch>
           <Route path="/" exact component={HomePage} />
           <Route path="/cart" component={CartPage} />
@@ -40,7 +41,7 @@ function App() {
           <Route path="/productList/:slug" component={ProductListPage} />
           <Route path="/viewProfile" component={ViewProfile} />
         </Switch>
-      </Router>
+      </Router> */}
     </div>
   );
 }
