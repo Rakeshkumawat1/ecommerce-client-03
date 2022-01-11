f48ac0636fa6567912636a513094d3b3ca3c21b5
Rakesh Kumawat <rakeskumarkumawat@gmail.com>
2022-01-11 13:23:18 +0530

    test

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -46,61 +46,62 @@ const HomePage = () => {
 
   const handleCheckNow = ({ slug, id }) => { history.push(`/${slug}/${id}/p`) }
   return (
+    <Layout/>
     // <Layout>
-    <div>
-      {allProducts.products.length ?
-        <div className="container" style={{ marginTop: "10px" }}>
-          <div className="row">
-            {allProducts.products.map((product) => (
-              <div className="col-sm-3" style={{ marginTop: "10px" }}>
-                <div className="card">
-                  <div className="caContainer">
-                    <Link className="caImgContainer" to={`/${product.slug}/${product._id}/p`} >
-                      <img classNameName="card-img-top" style={{ margin: "10px 0 0 30px", height: "250px", width: "200px" }} src={`${imgUrl}${product.productPictures[0].img}`} />
-                      {/* <img classNameName="card-img-top" style={{ margin: "10px 0 0 30px", height: "200px", width: "200px" }} src="https://ecommerce-server-01.herokuapp.com/public/f99d6T-57-download1.jpg" /> */}
-                    </Link>
-                  </div>
-                  <div className="card-body">
-                    <h5 className="card-title">{product.name}</h5>
-                    <BiRupee />
-                    {product.price}
-                    <span className="discountHome">
-                      22% off
-                    </span>
-                    <span className="ratingCountHome " >
-                      4.3 <IoIosStar />
-                    </span>
-                    <div>
-                      <button className="btn btn-primary" style={{ width: "100%" }} onClick={() => handleCheckNow({ slug: product.slug, id: product._id })}>Check now</button>
-                      {/* <button className="btn btn-primary" style={{ width: "100px" }}>Buy now</button>
-                      <button className="btn btn-success" style={{ width: "120px", marginLeft: "5px" }}>Add to cart</button> */}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-          {allProducts.loadMore ?
-            <div className='container text-center'>
-              <img src={loadingImg} style={{ width: "300px", height: "100px", marginTop: "auto" }} alt="Loading..." />
-            </div>
-            :
-            <div style={{ textAlign: "center", marginTop: "20px", color: "blue" }}> <h6>No more data!</h6></div>
-          }
-        </div>
-        :
-        // allProducts.loadMore ?
-        //   <div className='container text-center'>
-        //     <img src={loadingImg} style={{ width: "300px", height: "300px", marginTop: "auto" }} alt="Loading..." />
-        //   </div>
-        //   :
-        //   <div style={{ textAlign: "center", marginTop: "20px", color: "blue" }}> <h6>No more data!</h6></div>
+    // {/* <div> */}
+    //   {allProducts.products.length ?
+    //     <div className="container" style={{ marginTop: "10px" }}>
+    //       <div className="row">
+    //         {allProducts.products.map((product) => (
+    //           <div className="col-sm-3" style={{ marginTop: "10px" }}>
+    //             <div className="card">
+    //               <div className="caContainer">
+    //                 <Link className="caImgContainer" to={`/${product.slug}/${product._id}/p`} >
+    //                   <img classNameName="card-img-top" style={{ margin: "10px 0 0 30px", height: "250px", width: "200px" }} src={`${imgUrl}${product.productPictures[0].img}`} />
+    //                   {/* <img classNameName="card-img-top" style={{ margin: "10px 0 0 30px", height: "200px", width: "200px" }} src="https://ecommerce-server-01.herokuapp.com/public/f99d6T-57-download1.jpg" /> */}
+    //                 </Link>
+    //               </div>
+    //               <div className="card-body">
+    //                 <h5 className="card-title">{product.name}</h5>
+    //                 <BiRupee />
+    //                 {product.price}
+    //                 <span className="discountHome">
+    //                   22% off
+    //                 </span>
+    //                 <span className="ratingCountHome " >
+    //                   4.3 <IoIosStar />
+    //                 </span>
+    //                 <div>
+    //                   <button className="btn btn-primary" style={{ width: "100%" }} onClick={() => handleCheckNow({ slug: product.slug, id: product._id })}>Check now</button>
+    //                   {/* <button className="btn btn-primary" style={{ width: "100px" }}>Buy now</button>
+    //                   <button className="btn btn-success" style={{ width: "120px", marginLeft: "5px" }}>Add to cart</button> */}
+    //                 </div>
+    //               </div>
+    //             </div>
+    //           </div>
+    //         ))}
+    //       </div>
+    //       {allProducts.loadMore ?
+    //         <div className='container text-center'>
+    //           <img src={loadingImg} style={{ width: "300px", height: "100px", marginTop: "auto" }} alt="Loading..." />
+    //         </div>
+    //         :
+    //         <div style={{ textAlign: "center", marginTop: "20px", color: "blue" }}> <h6>No more data!</h6></div>
+    //       }
+    //     </div>
+    //     :
+    //     // allProducts.loadMore ?
+    //     //   <div className='container text-center'>
+    //     //     <img src={loadingImg} style={{ width: "300px", height: "300px", marginTop: "auto" }} alt="Loading..." />
+    //     //   </div>
+    //     //   :
+    //     //   <div style={{ textAlign: "center", marginTop: "20px", color: "blue" }}> <h6>No more data!</h6></div>
 
-        <div className='container text-center'>
-          <img src={loadingImg} style={{ width: "300px", height: "300px", marginTop: "auto" }} alt="Loading..." />
-        </div>
-      }
-      </div>
+    //     <div className='container text-center'>
+    //       <img src={loadingImg} style={{ width: "300px", height: "300px", marginTop: "auto" }} alt="Loading..." />
+    //     </div>
+    //   }
+    //   {/* </div> */}
     // </Layout>
 
 
